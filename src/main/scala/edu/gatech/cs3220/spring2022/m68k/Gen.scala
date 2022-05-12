552ceb1a010956dc5c4e74a0035786851af08ce2
Ammar Ratnani <ammrat13@gmail.com>
2022-05-12 17:51:02 -0400

    Added reflection constant and round keys

--- src/main/scala/edu/gatech/cs3220/spring2022/m68k/Gen.scala
+++ src/main/scala/edu/gatech/cs3220/spring2022/m68k/Gen.scala
@@ -1,10 +1,22 @@
 package edu.gatech.cs3220.spring2022.m68k
 
-import chisel3.emitVerilog
+import chisel3._
 
 import edu.gatech.cs3220.spring2022.m68k.qarma_hw.QarmaHW
 
 /** Main program to generate the hardware */
 object Gen extends App {
-  emitVerilog(new QarmaHW, args)
+  emitVerilog(
+    new QarmaHW(
+      a = "h_c0ac29b7c97c50dd".U(64.W),
+      c = Seq(
+        "h_0000000000000000".U(64.W),
+        "h_13198a2e03707344".U(64.W),
+        "h_a4093822299f31d0".U(64.W),
+        "h_082efa98ec4e6c89".U(64.W),
+        "h_452821e638d01377".U(64.W)
+      )
+    ),
+    args
+  )
 }
--- src/main/scala/edu/gatech/cs3220/spring2022/m68k/qarma_hw/QarmaHW.scala
+++ src/main/scala/edu/gatech/cs3220/spring2022/m68k/qarma_hw/QarmaHW.scala
@@ -27,8 +27,25 @@ class QarmaHWOutput extends Bundle {
   *
   * It's designed to have four cycles of latency, implemented iteratively. Only
   * Qarma64 with five rounds is supported.
+  *
+  * @param a
+  *   The reflection constant
+  * @param c
+  *   The sequence of round keys
   */
-class QarmaHW extends Module {
+class QarmaHW(a: UInt, c: Seq[UInt]) extends Module {
+
+  // Reflection constant must be a 64-bit integer
+  require(
+    this.a.widthOption == Some(64),
+    "Reflection constant must be a 64-bit unsigned integer"
+  )
+  // Must have five round constants, all 64-bit integers
+  require(this.c.length >= 5, "Must have at least five round constants")
+  require(
+    this.c.forall(n => n.widthOption == Some(64)),
+    "All round constants must be 64-bit unsigned integers"
+  )
 
   val io = IO(new Bundle {
     val keys = Input(new QarmaHWKeys)
