4a9584016ebcefc9dab0b2bd413c39d2e3e0e51e
Ammar Ratnani <ammrat13@gmail.com>
2022-05-12 17:12:25 -0400

    Rename module

--- src/main/scala/edu/gatech/cs3220/spring2022/m68k/Gen.scala
+++ src/main/scala/edu/gatech/cs3220/spring2022/m68k/Gen.scala
@@ -2,9 +2,9 @@ package edu.gatech.cs3220.spring2022.m68k
 
 import chisel3.emitVerilog
 
-import edu.gatech.cs3220.spring2022.m68k.qarma_hw.Qarma
+import edu.gatech.cs3220.spring2022.m68k.qarma_hw.QarmaHW
 
 /** Main program to generate the hardware */
 object Gen extends App {
-  emitVerilog(new Qarma, args)
+  emitVerilog(new QarmaHW, args)
 }
--- src/main/scala/edu/gatech/cs3220/spring2022/m68k/qarma_hw/Qarma.scala
+++ src/main/scala/edu/gatech/cs3220/spring2022/m68k/qarma_hw/Qarma.scala
@@ -2,7 +2,7 @@ package edu.gatech.cs3220.spring2022.m68k.qarma_hw
 
 import chisel3._
 
-class Qarma extends Module {
+class QarmaHW extends Module {
 
   val inp = IO(Input(Bool()))
   val out = IO(Output(Bool()))
